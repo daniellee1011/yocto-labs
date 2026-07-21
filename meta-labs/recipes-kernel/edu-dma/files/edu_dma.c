6904a8d32cbdae78a154cc19f6f1bb65e0daa710
daniellee1011 <kauvoi1011@gmail.com>
2026-07-21 15:27:19 -0400

    Add coherent DMA buffer setup

--- meta-labs/recipes-kernel/edu-dma/files/edu_dma.c
+++ meta-labs/recipes-kernel/edu-dma/files/edu_dma.c
@@ -4,15 +4,22 @@
 #include <linux/pci.h>
 #include <linux/io.h>
 #include <linux/slab.h>
+#include <linux/dma-mapping.h>
+#include <linux/string.h>
 
-#define EDU_VENDOR_ID       0X1234
+#define EDU_VENDOR_ID       0x1234
 #define EDU_DEVICE_ID       0x11e8
 #define EDU_REG_ID          0x00
 #define EDU_REG_LIVENESS    0x04
+#define EDU_DMA_MASK_BITS   28
+#define EDU_DMA_BUFFER_SIZE 4096
 
 struct edu_device {
     struct pci_dev *pdev;
     void __iomem *bar0;
+
+    void *dma_buffer;   // CPU virtual address; CPU accesses it
+    dma_addr_t dma_handle;  // device/DMA address; hardware receives it
 };
 
 static int edu_probe(struct pci_dev *pdev,
@@ -33,6 +40,13 @@ static int edu_probe(struct pci_dev *pdev,
     if (ret) {
         dev_err(&pdev->dev, "Failed to enable PCI memory resources\n");
         return ret;
+    }
+
+    ret = dma_set_mask_and_coherent(&pdev->dev,
+            DMA_BIT_MASK(EDU_DMA_MASK_BITS));
+    if (ret) {
+        dev_err(&pdev->dev, "Failed to configure 28-bit DMA mask\n");
+        goto err_disable_device;
     }
 
     ret = pci_request_region(pdev, 0, "edu_dma");
@@ -59,11 +73,25 @@ static int edu_probe(struct pci_dev *pdev,
         goto err_iounmap;
     }
 
+    edu->dma_buffer = dma_alloc_coherent(&pdev->dev,
+            EDU_DMA_BUFFER_SIZE, &edu->dma_handle, GFP_KERNEL);
+    if (!edu->dma_buffer) {
+        dev_err(&pdev->dev, "Failed to allocate coherent DMA buffer\n");
+        ret = -ENOMEM;
+        goto err_iounmap;
+    }
+
+    memset(edu->dma_buffer, 0, EDU_DMA_BUFFER_SIZE);
+
+    pci_set_master(pdev);
+
     pci_set_drvdata(pdev, edu);
 
     dev_info(&pdev->dev, "BAR0 %pR\n", &pdev->resource[0]);
     dev_info(&pdev->dev, "identification=%#010x\n", identification);
     dev_info(&pdev->dev, "liveness=%#010x\n", liveness);
+    dev_info(&pdev->dev, "DMA CPU address=%p\n", edu->dma_buffer);
+    dev_info(&pdev->dev, "DMA bus address=%pad\n", &edu->dma_handle);
 
     return 0;
 
@@ -81,6 +109,11 @@ err_disable_device:
 
 static void edu_remove(struct pci_dev *pdev) {
     struct edu_device *edu = pci_get_drvdata(pdev);
+
+    pci_clear_master(pdev);
+
+    dma_free_coherent(&pdev->dev,
+            EDU_DMA_BUFFER_SIZE, edu->dma_buffer, edu->dma_handle);
 
     pci_iounmap(pdev, edu->bar0);
     pci_release_region(pdev, 0);
